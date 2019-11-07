dd62ebcbe6c05c9cb451b87f03d965f0354dc3cb
Per Kristian Svanberg <kristiansvanberg@gmail.com>
2019-11-07 15:26:52 +0100

    Completed the plugin with code for the callback function

--- public/js/wcm18-crappy-map-public.js
+++ public/js/wcm18-crappy-map-public.js
@@ -1,32 +1,59 @@
-(function( $ ) {
-	'use strict';
+let crappyMap,
+	geocoder,
+	infoWindow;
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+function initMap() {
+	let $ = jQuery,
+		$wrapper = $('#wcm18-crappy-map'),
+		address = $wrapper.data('address'),
+		$map = $wrapper.find('#crappy-map'),
+		map = $map.get(0);
 
-})( jQuery );
+	// Create new instance of Google Maps Geocoder
+	geocoder = new google.maps.Geocoder();
+
+	// Geocode the address `city, country`
+	geocoder.geocode({
+		address: address
+	}, function(results, status){
+		if (results.length > 0) {
+			let latLng = results[0].geometry.location;
+			crappyMap = createMap(map, latLng);
+
+			let contentString = '<div id="content">\n' +
+			'    <div id="siteNotice"></div>\n' +
+			'    <h1 id="firstHeading" class="firstHeading">Google Maps API är skoj :)</h1>\n' +
+			'    <div id="bodyContent">\n' +
+			'        <p>\n' +
+			'            <b>Hej allihopa!</b>\n' +
+			'            Här har vi nu en text som visas i en ruta när man klickar på markören :)<br>\n' +
+			'            Super bra om man vill visa information om något!\n' +
+			'        </p>\n' +
+			'    </div>\n' +
+			'</div>';
+
+			infowindow = new google.maps.InfoWindow({
+				content: contentString
+			});
+
+			let centerMarker = new google.maps.Marker({
+				position: latLng,
+				map: crappyMap,
+				animation: google.maps.Animation.DROP,
+			});
+
+			centerMarker.addListener('click', function() {
+				infowindow.open(crappyMap, centerMarker);
+			});
+		}
+	});
+}
+
+function createMap(mapElement, latLng, mapOptions) {
+	return new google.maps.Map(mapElement, {
+		zoom: 11,
+		center: latLng,
+		disableDefaultUI: true,
+		gestureHandling: 'none'
+	});
+}
